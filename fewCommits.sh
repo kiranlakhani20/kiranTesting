b739c0e7840a14f9a7fc9b29fa4e2a80f8e088ca
kiran.lakhani <kiran.lakhani@servicenow.com>
2022-03-10 14:16:00 +0530

    670001:randfile_670001

--- fewCommits.sh
+++ fewCommits.sh
@@ -1,7 +1,7 @@
 string=""
 x=1
-j=98000
-while [ $x -le 1 ]
+j=670000
+while [ $x -le 5000 ]
 do
  string="touch randfile$((j+x)) && git add . && git commit -m '$((j+x)):randfile_$((j+x))'"
  echo $string
